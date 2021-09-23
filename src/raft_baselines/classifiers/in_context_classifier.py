d15cdf5671eabc3f5b361d2d8694f73213ef7ca3
Eli Lifland <eli.d.lifland@gmail.com>
2021-09-23 10:42:38 -0400

    fix bugs in in_context_classifier

--- src/raft_baselines/classifiers/in_context_classifier.py
+++ src/raft_baselines/classifiers/in_context_classifier.py
@@ -225,7 +225,6 @@ class InContextClassifier(Classifier):
         prompt: str,
     ) -> Dict[str, float]:
         raw_p = self._get_raw_probabilities(prompt)
-        raw_p = torch.stack(raw_p).cpu().detach().numpy()
         sum_p = np.sum(raw_p)
         if sum_p > 0:
             normalized_p = np.array(raw_p) / np.sum(raw_p)
@@ -244,12 +243,9 @@ class InContextClassifier(Classifier):
     ) -> Dict[str, float]:
         ordered_target = {col: target[col] for col in self.input_cols if col in target}
 
-        example_dataset = self.select_training_examples(target, random_seed=random_seed)
-        # Uncomment below call and comment out above call to run without
-        # minor semantic search query formatting bug
-        # example_dataset = self.select_training_examples(
-        #     ordered_target, random_seed=random_seed
-        # )
+        example_dataset = self.select_training_examples(
+            ordered_target, random_seed=random_seed
+        )
 
         prompt = self.format_prompt(ordered_target, example_dataset)
         if should_print_prompt:
